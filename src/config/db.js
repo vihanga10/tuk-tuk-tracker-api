a5384878ecab01f2c223388b8228c8ecb86242cd
Vihanga Madushamini <163097987+vihanga10@users.noreply.github.com>
2026-05-01 15:38:41 +0530

    Update DB Connection

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,11 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      maxPoolSize: 10,
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(`DB Error: ${err.message}`);
